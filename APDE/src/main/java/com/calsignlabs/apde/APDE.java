629196777d4e63e3e87ca617b2b5c2486efae159
William Smith <calsignlabs@gmail.com>
2015-03-28 14:00:03 -0400

    Add an "Uninstall Sketch" tool

--- APDE/src/main/java/com/calsignlabs/apde/APDE.java
+++ APDE/src/main/java/com/calsignlabs/apde/APDE.java
@@ -32,6 +32,7 @@ import com.calsignlabs.apde.tool.ImportLibrary;
 import com.calsignlabs.apde.tool.IncreaseIndent;
 import com.calsignlabs.apde.tool.ManageLibraries;
 import com.calsignlabs.apde.tool.Tool;
+import com.calsignlabs.apde.tool.UninstallSketch;
 import com.calsignlabs.apde.vcs.GitRepository;
 
 import java.io.File;
@@ -1011,7 +1012,7 @@ public class APDE extends Application {
 		String[] coreTools = new String[] { AutoFormat.PACKAGE_NAME, ImportLibrary.PACKAGE_NAME, ManageLibraries.PACKAGE_NAME, ColorSelector.PACKAGE_NAME,
 				CommentUncomment.PACKAGE_NAME, IncreaseIndent.PACKAGE_NAME, DecreaseIndent.PACKAGE_NAME,
 				ExportEclipseProject.PACKAGE_NAME, ExportSignedPackage.PACKAGE_NAME,
-				GitManager.PACKAGE_NAME, FindReplace.PACKAGE_NAME};
+				GitManager.PACKAGE_NAME, FindReplace.PACKAGE_NAME, UninstallSketch.PACKAGE_NAME};
 		
 		for (String coreTool : coreTools) {
 			loadTool(tools, packageToToolTable, coreTool);
---
+++ APDE/src/main/java/com/calsignlabs/apde/tool/UninstallSketch.java
@@ -0,0 +1,83 @@
+package com.calsignlabs.apde.tool;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.net.Uri;
+import android.view.MenuItem;
+
+import com.calsignlabs.apde.APDE;
+import com.calsignlabs.apde.KeyBinding;
+import com.calsignlabs.apde.R;
+
+import java.util.List;
+import java.util.Locale;
+
+public class UninstallSketch implements Tool {
+	public static final String PACKAGE_NAME = "com.calsignlabs.apde.tool.UninstallSketch";
+	
+	private APDE context;
+	
+	@Override
+	public void init(APDE context) {
+		this.context = context;
+	}
+	
+	@Override
+	public String getMenuTitle() {
+		return context.getResources().getString(R.string.uninstall_sketch);
+	}
+	
+	@Override
+	public void run() {
+		String sketchName = context.getSketchName();
+		
+		if (isSketchInstalled(sketchName)) {
+			Intent uninstallIntent = new Intent(Intent.ACTION_DELETE, Uri.parse("package:" + getPackageName(sketchName)));
+			context.getEditor().startActivity(uninstallIntent);
+		} else {
+			AlertDialog.Builder builder = new AlertDialog.Builder(context.getEditor());
+			builder.setTitle(R.string.sketch_not_installed_dialog_title);
+			builder.setMessage(R.string.sketch_not_installed_dialog_message);
+			builder.setNeutralButton(R.string.ok, new DialogInterface.OnClickListener() {
+				@Override
+				public void onClick(DialogInterface dialog, int which) {}
+			});
+			
+			builder.create().show();
+		}
+	}
+	
+	@Override
+	public KeyBinding getKeyBinding() {
+		return null;
+	}
+	
+	@Override
+	public boolean showInToolsMenu(APDE.SketchLocation sketchLocation) {
+		return true;
+	}
+	
+	@Override
+	public boolean createSelectionActionModeMenuItem(MenuItem convert) {
+		return false;
+	}
+	
+	private boolean isSketchInstalled(String sketchName) {
+		List<PackageInfo> installedPackages = context.getPackageManager().getInstalledPackages(0);
+		String packageName = getPackageName(sketchName);
+		
+		for (PackageInfo packageInfo : installedPackages) {
+			if (packageInfo.packageName.equals(packageName)) {
+				return true;
+			}
+		}
+		
+		return false;
+	}
+	
+	private String getPackageName(String sketchName) {
+		return "processing.test." + sketchName.toLowerCase(Locale.US);
+	}
+}
